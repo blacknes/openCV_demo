91342990b0551b3839a5fc7c45354c9fbb9fe380
blacknes <2401363179@qq.com>
2016-04-18 16:35:52 +0800

    supplement push

---
+++ Source_code/2_TestInstallation/DisplayImage.cpp
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Author :          Nill_Rhoads
+ * Email :           nillrhoads@gmail.com
+ * Last modified :   2016-04-18 16:19
+ * Filename :        DisplayImage.cpp
+ * Description :     
+ * *****************************************************************************/
+
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main(int argc, char** argv )
+{
+    if ( argc != 2 )
+    {
+        printf("usage: DisplayImage.out <Image_Path>\n");
+        return -1;
+    }
+
+    Mat image;
+    image = imread( argv[1], 1 );
+
+    if ( !image.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+    namedWindow("Display Image", WINDOW_AUTOSIZE );
+    imshow("Display Image", image);
+
+    waitKey(0);
+
+    return 0;
+}
